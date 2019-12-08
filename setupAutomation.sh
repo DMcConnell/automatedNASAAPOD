9889b2b49c31f81fc1de2b3b7a901a0d9ac12a2c
Duncan McConnell <ddmcconnell4@gmail.com>
2019-12-07 21:41:59 -0600

    Updated timeline

--- setupAutomation.sh
+++ setupAutomation.sh
@@ -1,5 +1,5 @@
 
 crontab -l | grep -v 'worker.sh'  | crontab -
-crontab -l | { cat; echo "40 21 * * * $PWD/worker.sh $PWD"; } | crontab -
+crontab -l | { cat; echo "45 21 * * * $PWD/worker.sh $PWD"; } | crontab -
 
 crontab -l
