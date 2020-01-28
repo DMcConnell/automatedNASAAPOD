1d0d7a8f89086fdbdd0f470e95c8ef238bf02479
Duncan McConnell <ddmcconnell4@gmail.com>
2020-01-28 12:45:55 -0600

    Updated to add some logging mechanics

--- worker.sh
+++ worker.sh
@@ -8,6 +8,10 @@ if [ ! -d "$1/$IMAGE_PATH" ]; then
     mkdir $1/$IMAGE_PATH
 fi
 
+if [ ! -d "$1/logs" ]; then
+    mkdir $1/logs
+fi
+
 # Downloads nasa picture and mv to images folder
-python $1/src/download.py $1
+python $1/src/download.py $1 >> $1/logs/status.log
 mv $1/*.jpg $1/$IMAGE_PATH
